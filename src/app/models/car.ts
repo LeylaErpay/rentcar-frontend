bc6f7b314774498b7e33c9d8859a379f1482d116
Leyla Erpay <leylakzl89@gmail.com>
2021-03-18 20:16:00 +0300

    10/3/2021

---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car {
+    Id: number;
+    brandName: string;
+    modelName: string;
+    modelYear: string;
+    colorName: string;
+    dailyPrice: number;
+    descriptions: string;
+  }
